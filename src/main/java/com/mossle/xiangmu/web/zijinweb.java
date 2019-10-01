7b6a4c771f159d645d560fb33fdc4cd888a4d9a0
Administrator <13837509562@163.com>
2019-10-01 18:16:21 +0800

    增加获取用户名

--- src/main/java/com/mossle/xiangmu/web/zijinweb.java
+++ src/main/java/com/mossle/xiangmu/web/zijinweb.java
@@ -3,6 +3,8 @@ package com.mossle.xiangmu.web;
 import com.mossle.xiangmu.persistence.manager.kemuManager;
 import com.mossle.xiangmu.persistence.manager.zijinmanager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +26,9 @@ public class zijinweb {
     @RequestMapping("xmjc")
     @ResponseBody
     public Object input() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String name = auth.getName(); //主体名，即登录用户名
+        System.out.println(name);//saysky 或 anonymousUser
         Map<String, Object> resultMap = new HashMap<>();
         resultMap.put("records", "1000000");
         resultMap.put("page", "6");
